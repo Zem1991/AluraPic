77bf6e0d795f12354453ead26c586db99e991770
Zem1991 <felipelopeszem@hotmail.com>
2019-02-07 21:42:41 -0200

    Alura Parte 1 Aula 6

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Photo } from './photo';
 
 const API = 'http://localhost:3000';
@@ -13,6 +13,13 @@ export class PhotoService {
 
   listFromUser(userName: string) {
     return this.http
-    .get<Photo[]>(API + '/' + userName + '/photos');
+      .get<Photo[]>(API + '/' + userName + '/photos');
+  }
+
+  listFromUserPaginated(userName: string, page: number) {
+    const params = new HttpParams()
+      .append('page', page.toString());
+    return this.http
+      .get<Photo[]>(API + '/' + userName + '/photos', { params });
   }
 }
